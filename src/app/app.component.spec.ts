8dcff47527506dc3d410991b365991a447bb171f
JE72539 <sundaramanikandan.kandaswamy@kbc.be>
2021-07-05 10:25:32 +0530

    Sudoku Game - Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -26,6 +26,80 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sudoku-app app is running!');
+    expect(compiled.querySelector('.card span').textContent).toContain('sudoku-app');
   });
+
+  it('should render the board', () => {
+    const fixture = TestBed.createComponent(AppComponent);fixture.detectChanges();
+    const app = fixture.componentInstance;
+    let button = fixture.debugElement.nativeElement.querySelector('.sudoku-header button');
+    button.click();
+    expect(app.board[0]).toBeTruthy();
+    button.click();
+    expect(app.board[0]).toBeTruthy();
+    button.click();
+    expect(app.board[0]).toBeTruthy();
+  });
+
+  it('should highlight the selected number from the cell', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.startGame();
+    app.selectedValue(1);
+    expect(app.hightlightNumber[1]).toBeTruthy();
+    app.selectedValue(1);
+    expect(app.hightlightNumber[1]).toBeFalsy();
+  });
+
+  it('should highlight the selected Tile from the board', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.startGame();
+    app.selectedTile(1);
+    expect(app.hightlightTile[1]).toBeTruthy();
+    app.selectedTile(1);
+    expect(app.hightlightTile[1]).toBeFalsy();
+  });
+
+  it('should display the value on the board', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.startGame();
+    app.selectedTile(1);
+    app.selectedValue(8);
+    expect(app.board[0].charAt(+1)).toEqual(app.board[1].charAt(+1));
+  });
+
+  it('should complete the game', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.startGame();
+    app.board =  [
+      "6-5329174971485326234761859362574981549618732718293465823946517197852643456137298",
+      "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
+    ];
+    app.selectedTile(1);
+    app.selectedValue(8);
+    expect(app.board[0].charAt(+1)).toEqual(app.board[1].charAt(+1));
+    expect(app.isWon).toBeTruthy();
+  });
+
+  it('should not display the value on the board', fakeAsync(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    app.startGame();
+    app.selectedTile(1);
+    app.selectedValue(9);
+    tick(1501);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.board[0].charAt(+1)).not.toEqual(app.board[1].charAt(+1));
+    });
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BOARD_LENGTH, EASY, HARD, MEDIUM, NUMBER_SEQUENCE} from "../shared/constants";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'sudoku-app';
+  public selectedLevel: string = "";
+  public board = ["", ""];
+  public boardLength = BOARD_LENGTH;
+  public numbersequence = NUMBER_SEQUENCE;
+  public hightlightNumber: Array<boolean> = [];
+  public hightlightTile: Array<boolean> = [];
+  public highlightIncorrect: Array<boolean> = [];
+  public isWon = false;
+  private previousNumber: number = 0;
+  private previousTile: number = 0;
+  private selectedNumber: string = "";
+  private selectedTileValue: string = "";
+
+  public startGame(): void {
+    if (!this.selectedLevel || this.selectedLevel === 'hard') {
+      this.board = [...EASY];
+      this.selectedLevel = 'easy';
+    } else if (this.selectedLevel === 'easy') {
+      this.board = [...MEDIUM];
+      this.selectedLevel = 'medium';
+    } else {
+      this.board = [...HARD];
+      this.selectedLevel = 'hard';
+    }
+    this.resetAll();
+  }
+
+  public selectedValue(cellValue: number): void {
+    if (this.previousNumber && this.previousNumber === cellValue) {
+      this.hightlightNumber[cellValue] = !this.hightlightNumber[cellValue];
+      this.selectedNumber = "";
+    } else {
+      this.previousNumber = cellValue;
+      this.hightlightNumber = [];
+      this.hightlightNumber[cellValue] = true;
+      this.selectedNumber = cellValue.toString();
+    }
+    this.updateMove();
+  }
+
+  public selectedTile(boardValue: number): void {
+    if (this.previousTile && this.previousTile === boardValue) {
+      this.hightlightTile[boardValue] = !this.hightlightTile[boardValue];
+      this.selectedTileValue = "";
+    } else {
+      this.previousTile = boardValue;
+      this.hightlightTile = [];
+      this.hightlightTile[boardValue] = true;
+      this.selectedTileValue = boardValue.toString();
+    }
+    this.updateMove();
+  }
+
+  private updateMove(): void {
+    // If number and tile are selected
+    if (this.selectedTileValue && this.selectedNumber) {
+      this.board[0] = this.board[0].substring(0, +this.selectedTileValue) + this.selectedNumber + this.board[0].substring(+this.selectedTileValue + 1);
+      // For incorrect scenario
+      if (this.board[1].charAt(+this.selectedTileValue) !== this.board[0].charAt(+this.selectedTileValue)) {
+        this.highlightIncorrect[+this.selectedTileValue] = true;
+        let that = this;
+        setTimeout(function () {
+          that.board[0] = that.board[0].substring(0, +that.selectedTileValue) + '-' + that.board[0].substring(+that.selectedTileValue + 1);
+          that.resetAll();
+        }, 1500);
+      } else {
+        this.resetAll();
+        if(this.board[0] === this.board[1]) {
+          this.isWon = true;
+        }
+      }
+    }
+  }
+
+  private resetAll(): void {
+    this.resetNumber();
+    this.resetTile();
+    this.resetPreviousSelection();
+    this.isWon = false;
+  }
+
+  private resetPreviousSelection(): void {
+    this.previousTile = 0;
+    this.previousNumber = 0;
+  }
+
+  private resetTile(): void {
+    this.highlightIncorrect = [];
+    this.hightlightTile = [];
+    this.selectedTileValue = "";
+  }
+
+  private resetNumber(): void {
+    this.hightlightNumber = [];
+    this.selectedNumber = "";
+  }
 }
