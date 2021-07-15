f1dc0270577d5072219db48c765ee913ba79671a
JE72539 <sundaramanikandan.kandaswamy@kbc.be>
2021-07-15 10:19:22 +0530

    Added sudoku from npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DemoLibModule} from "@sundarit2007/demo-lib-sudoku";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
